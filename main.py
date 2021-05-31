0fb1d5b23ae6a8d81aee73e161319721e9b0961b
bsanket16 <bsankt16@gmail.com>
2021-05-31 10:59:03 +0530

    changed symbols/tickers to company name

--- main.py
+++ main.py
@@ -5,6 +5,15 @@ import yfinance as yf
 from fbprophet import Prophet
 from fbprophet.plot import plot_plotly
 from plotly import graph_objs as go
+import pandas as pd
+
+@st.cache
+def get_data():
+    path = 'stock.csv'
+    return pd.read_csv(path, low_memory=False)
+
+df = get_data()
+df = df.drop_duplicates(subset="Name", keep="first")
 
 START = "2015-01-01"
 TODAY = date.today().strftime("%Y-%m-%d")
@@ -12,10 +21,14 @@ TODAY = date.today().strftime("%Y-%m-%d")
 st.title("Stock Prediction")
 st.write("###")
 
-stocks = ("AAPL", "NFLX", "GOOG", "MSFT", "INFY", "RPOWER.NS", "BAJFINANCE.NS", "YESBANK.NS", "RCOM.NS", "EXIDEIND.NS", "TATACHEM.NS", "TATAMOTORS.NS", "RUCHI.NS")
+stocks = df['Name']
+# stocks = ("AAPL", "NFLX", "GOOG", "MSFT", "INFY", "RPOWER.NS", "BAJFINANCE.NS", "YESBANK.NS", "RCOM.NS", "EXIDEIND.NS", "TATACHEM.NS", "TATAMOTORS.NS", "RUCHI.NS")
 selected_stock = st.selectbox("Select dataset and years for prediction", stocks)
 
-n_years = st.slider("", 1, 4)
+index = df[df["Name"]==selected_stock].index.values[0]
+symbol = df["Symbol"][index]
+
+n_years = st.slider("", 1, 5)
 period = n_years * 365
 
 @st.cache
@@ -25,7 +38,7 @@ def load_data(ticker):
     return data
 
 data_load_state = st.text("Load data ...")
-data = load_data(selected_stock)
+data = load_data(symbol)
 data_load_state.text("Loading data ... Done!")
 
 st.write("###")
